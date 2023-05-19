a714181dfd99066bb5326621345ad91960d7c382
Kevin <Kevin@Mac-Kevin.local>
2023-05-18 18:58:55 -0600

    tarea 2 kevin ovalle

---
+++ Tarea2/Kevin Ovalle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class ComisionCalculator
+{
+    static void Main()
+    {
+
+        Console.WriteLine("nombre de vendedor: ");
+        string nombre = Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine("total de venta: ");
+        double totalVenta = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("porcentaje de comision: ");
+        int totalPorcentaje = Convert.ToInt32(Console.ReadLine());
+
+        double totalComision = totalVenta * totalPorcentaje / 100;
+
+        Console.WriteLine("total de comision es: Q" + totalComision.ToString("0.00"));
+
+
+    }
+
+}
