f963ab48a552938a9184ad83b9ed929caf53e366
Ludin López <llopez@ricza.local>
2023-05-25 10:21:04 -0600

    Tarea 5 Ludin Lopez

---
+++ Tarea5/Ludin Lopez/PasswordGenerator.cs
@@ -0,0 +1,39 @@
+namespace RandomPasswordGenerator
+{
+    
+    class PasswordGenerator
+    {
+        public static string GeneratePassword(int length, bool includeNumbers, bool includeSpecialCharacters)
+        {
+            // Create a random number generator.
+            System.Random rng = new System.Random();
+            //Random rng = new Random();
+            //RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+
+            // Create a string of all possible characters.
+            string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+<,>.";
+
+            // If numbers are allowed, add them to the string of possible characters.
+            if (includeNumbers)
+            {
+                characters += "0123456789";
+            }
+
+            // If special characters are allowed, add them to the string of possible characters.
+            if (includeSpecialCharacters)
+            {
+                characters += "!@#$%^&*()-_=+<,>.";
+            }
+
+            // Generate a random password of the specified length.
+            string password = "";
+            for (int i = 0; i < length; i++)
+            {
+                password += characters[rng.Next(1,length)];
+            }
+
+            // Return the generated password.
+            return password;
+        }
+    }
+}
---
+++ Tarea5/Ludin Lopez/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace RandomPasswordGenerator
+{
+    
+    
+
+    class ConsoleUI:PasswordGenerator
+    {
+        public static void Main()
+        {
+            //PasswordGenerator gpass = new PasswordGenerator();
+            // Prompt the user for the number of characters.
+            Console.WriteLine("How many characters should the password be?");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            // Prompt the user whether to include numbers.
+            Console.WriteLine("Do you want to include numbers in the password? (Y/N)");
+            string includeNumbers = Console.ReadLine();
+
+            // Prompt the user whether to include special characters.
+            Console.WriteLine("Do you want to include special characters in the password? (Y/N)");
+            string includeSpecialCharacters = Console.ReadLine();
+
+            // Generate a random password and display it to the user.
+            bool numbers = includeNumbers.ToLower() == "y";
+            bool specialCharacters = includeSpecialCharacters.ToLower() == "y";
+            string password =  GeneratePassword(length, numbers, specialCharacters);//gpass.GeneratePassword(length, numbers, specialCharacters);
+            Console.WriteLine("Your password is: " + password);
+            
+        }
+    }
+
+
+}
